677d922bdbc32ed8ece62e16e840128939561b40
Nicolas Bazille <oltodo@msn.com>
2024-06-08 22:39:24 +0200

    Allow to access app locally

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-const strapi = require('@strapi/strapi');
+var http = require("http");
+var httpProxy = require("http-proxy");
+
+PhusionPassenger.configure({ autoInstall: false });
+
+const proxy = httpProxy.createProxy();
+
+http
+  .createServer(function (req, res) {
+    proxy.web(req, res, {
+      target: "http://0.0.0.0:1337",
+    });
+  })
+  .listen("passenger");
+
+const strapi = require("@strapi/strapi");
 strapi.createStrapi().start();
