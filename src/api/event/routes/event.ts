64a0e027e61b9e1254986a2fefdf8d200934294d
Nicolas Bazille <oltodo@msn.com>
2026-03-12 22:06:49 +0100

    Introduce privacy levels

--- src/api/event/routes/event.ts
+++ src/api/event/routes/event.ts
@@ -4,4 +4,13 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter("api::event.event");
+export default factories.createCoreRouter("api::event.event", {
+  config: {
+    find: {
+      policies: ["events-privacy"],
+    },
+    findOne: {
+      policies: ["events-privacy"],
+    },
+  },
+});
